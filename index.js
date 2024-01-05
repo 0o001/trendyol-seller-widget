752f94286b4b87befaf018db482759d954da7006
Mustafa UZUN <mustafauzun0@gmail.com>
2024-01-05 21:53:12 +0300

    chore: add getter functions

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 (function() {
-    const followAPI = 'https://public.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/follow/';
-    const storeUrlAPI = ' https://public-sdc.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/seller-store/getStoreUrl';
-    const getAvailableCouponsAPI = 'https://public.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/coupon/getAvailableCoupons';
+    const TRENDYOL_API_ENDPOINTS = {
+        follow: 'https://public.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/follow/',
+        storeUrl: ' https://public-sdc.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/seller-store/getStoreUrl',
+        availableCoupons: 'https://public.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/coupon/getAvailableCoupons',
+        profile: 'https://public-mdc.m.trendyol.com/discovery-sellerstore-mobilewebgw-service/seller-profile/magaza/profil',
+    };
 
+    Object.freeze(TRENDYOL_API_ENDPOINTS);
+
+    function getFollowers(sellerId) {
+
+    }
+
+    function getStoreUrl(sellerId) {
+
+    }
+    
+    function getAvailableCoupons(sellerId) {
+
+    }
+
+    function getProfile(sellerId) {
+
+    }
 })();
