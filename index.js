2e9d3612d549f1830ff078d5759d44360c913ed0
Mustafa Uzun <mustafauzun0@gmail.com>
2024-01-05 17:21:55 +0300

    chrore: initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+(function() {
+    const followAPI = 'https://public.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/follow/';
+    const storeUrlAPI = ' https://public-sdc.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/seller-store/getStoreUrl';
+    const getAvailableCouponsAPI = 'https://public.trendyol.com/discovery-sellerstore-mobilewebgw-service/api/coupon/getAvailableCoupons';
+
+})();
